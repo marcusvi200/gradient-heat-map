b48a31f1253f9a9f3df146f9bc88d458aea8797e
Marcus Vinicius <marcusvi200@gmail.com>
2020-02-11 00:08:16 -0300

    Added the comment of the class's midForce parameter and changed the README

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Gradient Heat Map
-Essa class altera, na forma de uma cor gradiente, com três cores, de acordo com a porcentagem informada, um objeto passado em sua construção.
+# Gradient Heat Map - Version 0.9.11
+
+## Browser Support
+
+![Chrome](https://raw.github.com/alrra/browser-logos/master/src/chrome/chrome_48x48.png) | ![Firefox](https://raw.github.com/alrra/browser-logos/master/src/firefox/firefox_48x48.png) | ![Safari](https://raw.github.com/alrra/browser-logos/master/src/safari/safari_48x48.png) | ![Opera](https://raw.github.com/alrra/browser-logos/master/src/opera/opera_48x48.png) | ![Edge](https://raw.github.com/alrra/browser-logos/master/src/edge/edge_48x48.png) |
+--- | --- | --- | --- | --- |
+Latest ✔ | Latest ✔ | Latest ✔ | Latest ✔ | Latest ✔ |
+
+## How to use
+
+### Instantiating a GradientHeatMap
+
+```js
+var divEl = document.getElementById('barColor');
+var gradientHeatMap = new GradientHeatMap(divEl);
+```
+
+### Run the Gradient in Object
+
+```js
+let percentage = 50;
+let startColor = "#008000";
+let middleColor = "#ffff00";
+let endColor = "#ff0000";
+
+gradientHeatMap.run(position_GradientHeatMap.right,percentage,startColor,middleColor,endColor);
+```
+
+### Position types
+| Position | Direction |
+|----------|-----------|
+| **position_GradientHeatMap.top** | To top |
+| **position_GradientHeatMap.right** | To right |
+| **position_GradientHeatMap.left** | To left |
+| **position_GradientHeatMap.bottom** | To bottom |
+| **position_GradientHeatMap.top_right** | Diagonal from right to top |
+| **position_GradientHeatMap.top_left** | Diagonal from left to top |
+| **position_GradientHeatMap.bottom_right** | Diagonal from right to bottom |
+| **position_GradientHeatMap.bottom_left** | Diagonal from left to bottom |
--- script/GradientHeatMap.js
+++ script/GradientHeatMap.js
@@ -1,6 +1,8 @@
-class GradientHeatMap{
+class GradientHeatMap {
     /**
      * @author Marcus Vinicius Oliveira Silva
+     * @version 0.9.11
+     * @description Added the comment of the class's midForce parameter and changed the README
      * @version 0.9.10
      * @description This class changes, in the form of a gradient color, according to the informed percentage, an object passed in its construction.
      * This object must be an element and cannot be text.
@@ -18,9 +20,10 @@ class GradientHeatMap{
      * @param {Boolean} displayPercentage
      * @param {position_GradientHeatMap} position 
      * @param {Number} minForce 
+     * @param {Number} midForce 
      * @param {Number} maxForce 
      */
-    constructor(object,startColor = "#ffffff",middleColor = "#999999",endColor = "#000000",percentage = 0,displayPercentage = false,position = position_GradientHeatMap.right,minForce = 300,midForce = 100,maxForce = 0){
+    constructor(object, startColor = "#ffffff", middleColor = "#999999", endColor = "#000000", percentage = 0, displayPercentage = false, position = position_GradientHeatMap.right, minForce = 300, midForce = 100, maxForce = 0) {
         this._object = object;
 
         this._startColor = startColor;
@@ -40,11 +43,11 @@ class GradientHeatMap{
         this.initialize();
     }
 
-    initialize(){
-        if(this._checkColors() && this._checkObject() && this._checkPercentage()){
+    initialize() {
+        if (this._checkColors() && this._checkObject() && this._checkPercentage()) {
             let obj = this._object.getElementsByClassName("F9Qyztg9KHNg_class");
-            
-            if(obj.length == 0){
+
+            if (obj.length == 0) {
                 let elementDiv = document.createElement('DIV');
                 elementDiv.className = "F9Qyztg9KHNg_class";
                 elementDiv.style.position = "absolute";
@@ -54,7 +57,7 @@ class GradientHeatMap{
             }
 
             return this;
-        }else{
+        } else {
             console.error('Impossible to continue.', 'Check the parameters!');
         }
     }
@@ -67,25 +70,25 @@ class GradientHeatMap{
      * @param {String} middleColor
      * @param {String} endColor
      */
-    run(position = this._position,percentage = this._percentage,startColor = this._startColor,middleColor = this._middleColor,endColor = this._endColor){
+    run(position = this._position, percentage = this._percentage, startColor = this._startColor, middleColor = this._middleColor, endColor = this._endColor) {
         this._position = position;
         this._percentage = percentage;
         this._startColor = startColor;
         this._middleColor = middleColor;
         this._endColor = endColor;
 
-        if(this._checkColors() && this._checkObject() && this._checkPercentage()){
-            let minPercent = this._map(percentage,Math.abs(this._maxForce)*-1,Math.abs(this._minForce));
-            let midPercent = this._map(percentage,Math.abs(this._midForce)*-1,Math.abs(this._maxForce));
-            let maxPercent = this._map(percentage,Math.abs(this._minForce)*-1,Math.abs(this._maxForce));
+        if (this._checkColors() && this._checkObject() && this._checkPercentage()) {
+            let minPercent = this._map(percentage, Math.abs(this._maxForce) * -1, Math.abs(this._minForce));
+            let midPercent = this._map(percentage, Math.abs(this._midForce) * -1, Math.abs(this._maxForce));
+            let maxPercent = this._map(percentage, Math.abs(this._minForce) * -1, Math.abs(this._maxForce));
 
             this._object.style.background = `linear-gradient(${position}, ${this._endColor} ${minPercent}%, ${this._middleColor} ${midPercent}%, ${this._startColor} ${maxPercent}%)`;
 
             let obj = this._object.getElementsByClassName("F9Qyztg9KHNg_class");
             obj[0].innerHTML = (percentage != null ? `${percentage}%` : `0%`);
             obj[0].style.color = `linear-gradient(${position}, ${this._endColor} ${minPercent}%, ${this._middleColor} ${midPercent}%, ${this._startColor} ${maxPercent}%)`;
-           
-        }else{
+
+        } else {
             console.error('Impossible to continue.', 'Check the parameters!');
         }
     }
@@ -96,24 +99,24 @@ class GradientHeatMap{
      * @param {Number} in_min 
      * @param {Number} in_max 
      */
-    _map(percentage, in_min, in_max){
-        return (((in_max-in_min)*(percentage/100)+in_min)).toFixed(0);
+    _map(percentage, in_min, in_max) {
+        return (((in_max - in_min) * (percentage / 100) + in_min)).toFixed(0);
     }
 
-    _checkColors(){
-        if(typeof this._startColor != 'string' || typeof this._middleColor != 'string' || typeof this._endColor != 'string' ||
-           this._startColor.substr(0,1) != '#' || this._middleColor.substr(0,1) != '#' || this._endColor.substr(0,1) != '#' ||
-           this._startColor.length != 7 || this._middleColor.length != 7 || this._endColor.length != 7){
+    _checkColors() {
+        if (typeof this._startColor != 'string' || typeof this._middleColor != 'string' || typeof this._endColor != 'string' ||
+            this._startColor.substr(0, 1) != '#' || this._middleColor.substr(0, 1) != '#' || this._endColor.substr(0, 1) != '#' ||
+            this._startColor.length != 7 || this._middleColor.length != 7 || this._endColor.length != 7) {
 
-           console.error('Color is not string!', `Color should be for example: #FFFFFF`);
+            console.error('Color is not string!', `Color should be for example: #FFFFFF`);
             return false;
         }
 
         return true;
     }
 
-    _checkObject(){
-        if(typeof this._object != 'object'){
+    _checkObject() {
+        if (typeof this._object != 'object') {
             console.error('Object is not object.', `Object is: ${typeof this._object}`);
             return false;
         }
@@ -121,39 +124,40 @@ class GradientHeatMap{
         return true;
     }
 
-    _checkPercentage(){
-        if(this._percentage >= 0 && this._percentage <= 100 && this._percentage != null){
+    _checkPercentage() {
+        if (this._percentage >= 0 && this._percentage <= 100 && this._percentage != null) {
             return true;
-        }else{
+        } else {
             console.error('Percent must be between 0 to 100.', `Percentage is with: ${this._percentage}`);
             return false;
         }
     }
 
-    get object(){
+    get object() {
         return this._object;
     }
 
-    get startColor(){
+    get startColor() {
         return this._startColor;
     }
 
-    get endColor(){
+    get endColor() {
         return this._endColor;
     }
 
-    get value(){
+    get value() {
         return this._percentage;
     }
-    
+
 }
 
-const position_GradientHeatMap = {'top': 'to top',
-                          'right': 'to right',
-                          'left': 'to left',
-                          'bottom': 'to bottom',
-                          'top_right': 'to top right',
-                          'top_left': 'to top left',
-                          'bottom_right': 'to bottom right',
-                          'bottom_left': 'to bottom left'                          
+const position_GradientHeatMap = {
+    'top': 'to top',
+    'right': 'to right',
+    'left': 'to left',
+    'bottom': 'to bottom',
+    'top_right': 'to top right',
+    'top_left': 'to top left',
+    'bottom_right': 'to bottom right',
+    'bottom_left': 'to bottom left'
 };
